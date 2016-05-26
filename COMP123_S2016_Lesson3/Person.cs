6a8bafa7574bc38a2fe209d17488be8835bf1bc1
HKobe <HGKobeissi1@gmail.com>
2016-05-26 15:32:06 -0400

    Lab2-Objects&Classes

---
+++ COMP123_S2016_Lesson3/Person.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2016_Lesson3
+{
+    class Person
+    {
+        #region Fields
+        // PRIVATE INSTANCE VARIABLES ++++++++++++++++++++++++++++++++++
+        private string _name;
+        private int _age;
+        #endregion
+
+        #region Properties
+
+        // PUBLIC PROPERTIES ++++++++++++++++++++++++++++++++++++++++++
+
+        /**
+         * This is a property for our _name field
+         * 
+         * @property {string} Name
+         */
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+
+            set
+            {
+                this._name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this._age;
+            }
+            set
+            {
+                this._age = value;
+            }
+        }
+
+        //Getters 
+       // public string getName()
+        //{
+          //  return _name;
+        //}
+        //Setters
+        //public void setName(string name)
+        //{
+        //   this._name = name;
+       // }
+
+        #endregion
+        /**
+         * This is the default empty constructor for the Person class
+        * 
+        * @constructor Person
+        */
+        public Person()
+        {
+            this._name = "Tom";
+        }
+
+        /**
+        * This is a consructor that takes name as a parameter and passes 
+        * it to the _name private instance variable
+        * 
+         * @constructor Person
+         * @param {string} name
+         */
+
+        public Person(string name)
+        {
+            this._name = name;
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine(this._name + " says hello");
+        }
+
+        public void ShowAge()
+        {
+            Console.WriteLine(this._name + " is " +this._age + " years old.");//"Tom is 47 years old"
+        }
+    }
+}
--- COMP123_S2016_Lesson3/Program.cs
+++ COMP123_S2016_Lesson3/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace COMP123_S2016_Lesson3
 {
-    class Program
+    namespace COMP123_S2016_Lesson3
     {
-        static void Main(string[] args)
+        /**
+         * This class is the "driver" class for our Program
+          * 
+          * @class Program
+          */
+        public class Program
         {
+            /**
+             * the main method for our driver class Program
+             * 
+             * @method Main
+             * @param {sting[]} args
+            */
+            public static void Main(string[] args)
+            {
+                // create a new instance of the Person class
+                Person person = new Person("Tom");
+
+                Console.WriteLine(person.Name);
+
+                // set a value in the Name property of the Person class
+                person.Name = "Hassan";
+                person.Age = 47;
+
+                // get the Name value and print to console
+                Console.WriteLine(person.Name);
+
+                person.SayHello();
+                person.ShowAge();
+            }
         }
     }
 }
+
+  
